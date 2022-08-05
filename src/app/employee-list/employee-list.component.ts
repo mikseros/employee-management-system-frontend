a298a0a6b93849cbc7bb5e76bcf53c97f3fbaeb2
mikseros <mmadynski@gmail.com>
2022-08-05 18:23:51 +0200

    AngularService implementation&ConnectingWith back.

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,21 +11,16 @@ export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
 
-  constructor() { }
+  constructor(private employeesService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [{
-      "id": 1,
-      "firstName": "John",
-      "lastName": "Doe",
-      "emailId": "john@xxx.com"
-    },
-    {
-      "id": 2,
-      "firstName": "Jane",
-      "lastName": "Doe",
-      "emailId": "jane@xxx.com"
-    }];
+    this.getEmployees();
+  }
+
+  private getEmployees() {
+    this.employeesService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
   }
 
 }
