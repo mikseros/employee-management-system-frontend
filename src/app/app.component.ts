2bb89a54181a92625338d324f6798b06e08d8a6d
mikseros <mmadynski@gmail.com>
2022-08-07 12:01:38 +0200

    Titlte changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-management-system-frontend';
+  title = 'Angular 13 Spring Boot CRUD Full Stack App';
 }
