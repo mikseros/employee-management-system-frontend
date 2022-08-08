0ba5bf4c1eb2bfff8c71adb587c7d09a9efe037d
mikseros <mmadynski@gmail.com>
2022-08-08 11:01:08 +0200

    Populate update-employee form by the data from DB

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 
@@ -10,11 +10,18 @@ import { EmployeeService } from '../employee.service';
 })
 export class UpdateEmployeeComponent implements OnInit {
 
+  id!: number;
   employee: Employee = new Employee();
   constructor(private employeeService: EmployeeService,
+    private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employeeService.getEmployeeById(this.id).subscribe(data =>{
+      this.employee = data;
+    }, error => console.log(error));
   }
 
   saveEmployee() {
