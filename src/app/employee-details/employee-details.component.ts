005904700c70d9c8bee7dd098677282308b20bdb
mikseros <mmadynski@gmail.com>
2022-08-08 21:33:38 +0200

    'employee-details' page & connection with REST API

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  employee!: Employee;
+  constructor(private route: ActivatedRoute,
+    private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    });
   }
 
 }
